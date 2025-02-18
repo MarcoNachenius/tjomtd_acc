27676f8e7e5477d2da4f15d1ca8f7b975b52cb07
MarcoNachenius <marconachenius@gmail.com>
2025-02-18 22:30:59 +0100

    Update test_shb_movement.gd

--- unit_tests/scripts/projectiles/bullets/single_hit/test_shb_movement.gd
+++ unit_tests/scripts/projectiles/bullets/single_hit/test_shb_movement.gd
@@ -88,3 +88,32 @@ func test_isometric_movement_south_west():
     single_hit_bullet.queue_free()
     dummy_target.queue_free()
     await get_tree().process_frame  # Ensure they are removed before the next test
+
+
+## Test the movement of the bullet when the target is directly right of the bullet on the screen.
+func test_isometric_movement_north_east():
+    var single_hit_bullet = SingleHitBullet.new()
+    single_hit_bullet.set_speed(30)
+    single_hit_bullet.position = Vector2.ZERO  # Explicitly reset position
+
+    var dummy_target = CreepConstants.CreepPreloads[CreepConstants.CreepIDs.DEMON].instantiate()
+	# Place the target right (positive X).
+    dummy_target.position = Vector2(1000, 0)
+
+    add_child_autofree(single_hit_bullet)
+    add_child_autofree(dummy_target)
+
+    single_hit_bullet.set_target(dummy_target)
+    dummy_target.stun(10)
+
+    assert_eq(single_hit_bullet.position, Vector2.ZERO, "Bullet should start at (0, 0) after being added to the scene tree.")
+    assert_eq(dummy_target.position, Vector2(1000, 0), "Target should start at (1000, 0) after being added to the scene tree.")
+
+    await get_tree().process_frame
+
+    assert_almost_eq(single_hit_bullet.position.y, 0.0, 0.01, "Y-coordinate should remain 0.0")
+    assert_almost_eq(single_hit_bullet.position.x, 30.0, 0.01, "X-coordinate should be approximately 30.0 (moved right by 30 units).")
+
+    single_hit_bullet.queue_free()
+    dummy_target.queue_free()
+    await get_tree().process_frame  # Ensure they are removed before the next test
