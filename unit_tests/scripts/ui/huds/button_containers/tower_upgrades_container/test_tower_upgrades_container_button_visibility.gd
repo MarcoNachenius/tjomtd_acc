54f57395f16fb9815d260a985d61ce361ddecce6
MarcoNachenius <marconachenius@gmail.com>
2025-06-11 21:50:30 +0200

    Add tests for Sam Site and Lava Pool tower button visibility in upgrade container

--- unit_tests/scripts/ui/huds/button_containers/tower_upgrades_container/test_tower_upgrades_container_button_visibility.gd
+++ unit_tests/scripts/ui/huds/button_containers/tower_upgrades_container/test_tower_upgrades_container_button_visibility.gd
@@ -133,7 +133,7 @@ func test_upgrade_tower_button_appearance_from_towers_on_map_list():
 
 
 
-func test_upgrade_tower_button_appearance_awaiting_selection():
+func test_tombstone_lvl_1_button_appearance_awaiting_selection():
     # ================================================================
     #                     ** CREATE BLANK MAP **
     # ================================================================
@@ -214,5 +214,174 @@ func test_upgrade_tower_button_appearance_awaiting_selection():
     black_marble_lvl_1.queue_free()
     bismuth_lvl_1.queue_free()
     larimar_lvl_1.queue_free()
+    # Process frame to ensure all objects are cleaned up
+    await get_tree().process_frame
+
+func test_sam_site_lvl_1_button_appearance_awaiting_selection():
+    # ================================================================
+    #                     ** CREATE BLANK MAP **
+    # ================================================================
+    var test_game_map = GameMap.new()
+    # LINE_TD is used because it has no mandatory path stops
+    test_game_map.MAP_ID = MapConstants.MapID.LINE_TD
+    # Set the map's size to 10x10
+    test_game_map.MAP_HEIGHT = 10
+    test_game_map.MAP_WIDTH = 10
+    # Ensure start and end points don't interfere with placement validity
+    test_game_map.__path_start_point = Vector2i(0, 0)
+    test_game_map.__path_end_point = Vector2i(0, 19)
+    # Create test tileset
+    var test_tileset = TileSet.new()
+    test_tileset.tile_shape = TileSet.TILE_SHAPE_ISOMETRIC
+    test_tileset.tile_layout = TileSet.TILE_LAYOUT_DIAMOND_DOWN
+    test_tileset.tile_offset_axis = TileSet.TILE_OFFSET_AXIS_VERTICAL
+    test_tileset.tile_size = Vector2i(256, 128)
+    # Asssign the test tileset to the map
+    test_game_map.tile_set = test_tileset
+    # Add the test map to the scene
+    add_child_autofree(test_game_map)
+    # Process frame to create the map
+    await get_tree().process_frame
+    # ================================================================
+
+    # Create test instance of game map
+    # Process frame to ensure the game map is ready
+    await get_tree().process_frame
+
+    # Create test instance of random tower build hud
+    var test_random_tower_build_hud: RandomTowerBuildHUD = GameConstants.HUD_LOADS[GameConstants.HudTypes.RANDOM_TOWER_BUILD].instantiate()
+    # Set the game map for the random tower build hud
+    test_random_tower_build_hud.GAME_MAP = test_game_map
+    add_child_autofree(test_random_tower_build_hud)
+    # Process frame to ensure the random tower build hud is ready
+    await get_tree().process_frame
+
+    # Create towers required for upgrade to tombstone
+    var spinel_lvl_1: Tower = TowerConstants.BUILD_TOWER_PRELOADS[TowerConstants.TowerIDs.SPINEL_LVL_1].instantiate()
+    test_game_map.add_child(spinel_lvl_1)
+    var kunzite_lvl_1: Tower = TowerConstants.BUILD_TOWER_PRELOADS[TowerConstants.TowerIDs.KUNZITE_LVL_1].instantiate()
+    test_game_map.add_child(kunzite_lvl_1)
+    var sunstone_lvl_1: Tower = TowerConstants.BUILD_TOWER_PRELOADS[TowerConstants.TowerIDs.SUNSTONE_LVL_1].instantiate()
+    test_game_map.add_child(sunstone_lvl_1)
+
+    # Create simulated list of towers and assign to game map
+    var test_towers_awaiting_selection: Array[Tower] = [spinel_lvl_1, kunzite_lvl_1, sunstone_lvl_1]
+    test_game_map.__towers_awaiting_selection = test_towers_awaiting_selection
+
+    # Ensure upgrade container is not visible before selecting a tower
+    assert_false(test_random_tower_build_hud.TOWER_UPGRADES_CONTAINER.visible, "Tower upgrades container should not be visible")
+
+    # Simulate tower being selected
+    test_game_map.tower_selected.emit(spinel_lvl_1)
+
+    # Process frame to ensure the tower selected signal is processed
+    await get_tree().process_frame
+
+    # Ensure selected tower has been correctly set
+    assert_eq(test_random_tower_build_hud.__selected_tower, spinel_lvl_1, "Selected tower should be black marble level 1")
+
+    # Ensure the game map is in navigation mode
+    assert_eq(test_game_map.get_state(), GameMap.States.NAVIGATION_MODE, "Game map should be in navigation mode")
+
+    # Ensure upgrade manager thinks that upgrade to Sam Site is possible
+    assert_true(test_random_tower_build_hud.TOWER_UPGRADE_MANAGER.can_upgrade_to_tower(TowerConstants.TowerIDs.SAM_SITE_LVL_1, test_towers_awaiting_selection), "Upgrade to Sam Site should be possible")
+
+    # Upgrade towers container should now be visible
+    assert_true(test_random_tower_build_hud.TOWER_UPGRADES_CONTAINER.visible, "Tower upgrades container should be visible")
+
+    # Sam Site level 1 button should be visible
+    assert_true(test_random_tower_build_hud.TOWER_UPGRADES_CONTAINER.TOWER_ID_TO_BUTTON_DICT[TowerConstants.TowerIDs.SAM_SITE_LVL_1].visible, "Sam Site level 1 button should be visible")
+
+    # Clean up
+    test_game_map.queue_free()
+    test_random_tower_build_hud.queue_free()
+    spinel_lvl_1.queue_free()
+    kunzite_lvl_1.queue_free()
+    sunstone_lvl_1.queue_free()
+    # Process frame to ensure all objects are cleaned up
+    await get_tree().process_frame
+
+
+func test_lava_pool_lvl_1_button_appearance_awaiting_selection():
+    # ================================================================
+    #                     ** CREATE BLANK MAP **
+    # ================================================================
+    var test_game_map = GameMap.new()
+    # LINE_TD is used because it has no mandatory path stops
+    test_game_map.MAP_ID = MapConstants.MapID.LINE_TD
+    # Set the map's size to 10x10
+    test_game_map.MAP_HEIGHT = 10
+    test_game_map.MAP_WIDTH = 10
+    # Ensure start and end points don't interfere with placement validity
+    test_game_map.__path_start_point = Vector2i(0, 0)
+    test_game_map.__path_end_point = Vector2i(0, 19)
+    # Create test tileset
+    var test_tileset = TileSet.new()
+    test_tileset.tile_shape = TileSet.TILE_SHAPE_ISOMETRIC
+    test_tileset.tile_layout = TileSet.TILE_LAYOUT_DIAMOND_DOWN
+    test_tileset.tile_offset_axis = TileSet.TILE_OFFSET_AXIS_VERTICAL
+    test_tileset.tile_size = Vector2i(256, 128)
+    # Asssign the test tileset to the map
+    test_game_map.tile_set = test_tileset
+    # Add the test map to the scene
+    add_child_autofree(test_game_map)
+    # Process frame to create the map
+    await get_tree().process_frame
+    # ================================================================
+
+    # Create test instance of game map
+    # Process frame to ensure the game map is ready
+    await get_tree().process_frame
+
+    # Create test instance of random tower build hud
+    var test_random_tower_build_hud: RandomTowerBuildHUD = GameConstants.HUD_LOADS[GameConstants.HudTypes.RANDOM_TOWER_BUILD].instantiate()
+    # Set the game map for the random tower build hud
+    test_random_tower_build_hud.GAME_MAP = test_game_map
+    add_child_autofree(test_random_tower_build_hud)
+    # Process frame to ensure the random tower build hud is ready
+    await get_tree().process_frame
+
+    # Create towers required for upgrade to tombstone
+    var black_marble_lvl_2: Tower = TowerConstants.BUILD_TOWER_PRELOADS[TowerConstants.TowerIDs.BLACK_MARBLE_LVL_2].instantiate()
+    test_game_map.add_child(black_marble_lvl_2)
+    var spinel_lvl_2: Tower = TowerConstants.BUILD_TOWER_PRELOADS[TowerConstants.TowerIDs.SPINEL_LVL_2].instantiate()
+    test_game_map.add_child(spinel_lvl_2)
+    var spinel_lvl_3: Tower = TowerConstants.BUILD_TOWER_PRELOADS[TowerConstants.TowerIDs.SPINEL_LVL_3].instantiate()
+    test_game_map.add_child(spinel_lvl_3)
+
+    # Create simulated list of towers and assign to game map
+    var test_towers_awaiting_selection: Array[Tower] = [black_marble_lvl_2, spinel_lvl_2, spinel_lvl_3]
+    test_game_map.__towers_awaiting_selection = test_towers_awaiting_selection
+
+    # Ensure upgrade container is not visible before selecting a tower
+    assert_false(test_random_tower_build_hud.TOWER_UPGRADES_CONTAINER.visible, "Tower upgrades container should not be visible")
+
+    # Simulate tower being selected
+    test_game_map.tower_selected.emit(black_marble_lvl_2)
+
+    # Process frame to ensure the tower selected signal is processed
+    await get_tree().process_frame
+
+    # Ensure selected tower has been correctly set
+    assert_eq(test_random_tower_build_hud.__selected_tower, black_marble_lvl_2, "Selected tower should be black marble level 1")
+
+    # Ensure the game map is in navigation mode
+    assert_eq(test_game_map.get_state(), GameMap.States.NAVIGATION_MODE, "Game map should be in navigation mode")
+
+    # Ensure upgrade manager thinks that upgrade to Lava Pool is possible
+    assert_true(test_random_tower_build_hud.TOWER_UPGRADE_MANAGER.can_upgrade_to_tower(TowerConstants.TowerIDs.LAVA_POOL_LVL_1, test_towers_awaiting_selection), "Upgrade to Lava Pool should be possible")
+
+    # Upgrade towers container should now be visible
+    assert_true(test_random_tower_build_hud.TOWER_UPGRADES_CONTAINER.visible, "Tower upgrades container should be visible")
+
+    # Lava Pool level 1 button should be visible
+    assert_true(test_random_tower_build_hud.TOWER_UPGRADES_CONTAINER.TOWER_ID_TO_BUTTON_DICT[TowerConstants.TowerIDs.LAVA_POOL_LVL_1].visible, "Lava Pool level 1 button should be visible")
+
+    # Clean up
+    test_game_map.queue_free()
+    test_random_tower_build_hud.queue_free()
+    black_marble_lvl_2.queue_free()
+    spinel_lvl_2.queue_free()
+    spinel_lvl_3.queue_free()
     # Process frame to ensure all objects are cleaned up
     await get_tree().process_frame
