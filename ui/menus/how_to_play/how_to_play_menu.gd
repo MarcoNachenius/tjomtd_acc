6beae7f3133e02b0de620cc993505c58e081a24a
MarcoNachenius <marconachenius@gmail.com>
2026-03-14 15:05:49 +0100

    Fix hud audio manager

--- ui/menus/how_to_play/how_to_play_menu.gd
+++ ui/menus/how_to_play/how_to_play_menu.gd
@@ -1,16 +1,22 @@
 extends Control
 class_name HowToPlayMenu
 
-# MENU CHANGE BUTTONS
+###########
+# EXPORTS #
+###########
+@export_category("Menu Change Buttons")
 @export var RETURN_TO_MAIN_MENU_BUTTON: Button
 @export var TOWER_AND_SLATE_INFO_BUTTON: Button
 
+@export_category("Text Display Buttons")
 
+#####################
+# INHERITED METHODS #
+#####################
 func _ready() -> void:
 	assert(RETURN_TO_MAIN_MENU_BUTTON, "Main menu button not assigned")
 	assert(TOWER_AND_SLATE_INFO_BUTTON, "Tower and Slate Info button not assigned")
 	_connect_all_button_signals()
-
 
 #####################
 # SIGNAL CONNECTORS #
@@ -18,15 +24,23 @@ func _ready() -> void:
 func _connect_all_button_signals() -> void:
 	_connect_menu_change_buttons_signals()
 
-
-# Menu Change Buttons
-# -------------------
 func _connect_menu_change_buttons_signals() -> void:
 	RETURN_TO_MAIN_MENU_BUTTON.pressed.connect(_on_main_menu_button_pressed)
-	TOWER_AND_SLATE_INFO_BUTTON.pressed.connect(_on_tower_slate_stats_button_pressed)
+	TOWER_AND_SLATE_INFO_BUTTON.pressed.connect(_on_tower_slate_info_button_pressed)
 
+##################
+# SIGNAL METHODS #
+##################
+# -------------------
+# Menu Change Buttons
+# -------------------
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_packed(UIConstants.START_GAME_MENU_LOAD)
 
-func _on_tower_slate_stats_button_pressed() -> void:
+func _on_tower_slate_info_button_pressed() -> void:
 	get_tree().change_scene_to_packed(UIConstants.RECIPE_MENU_LOAD)
+
+# --------------------
+# Text Display Buttons
+# --------------------
+
